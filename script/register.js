1259050cfab83e89f81ef65a92a3e908e279de1a
Gavin <oiamo123@gmail.com>
2024-05-28 16:07:57 -0600

    few changes

--- script/register.js
+++ script/register.js
@@ -4,6 +4,9 @@ const inputs = document.querySelectorAll(`input`);
 const [...inputs1] = inputs;
 const submitButton = document.querySelector(`.submit--button`);
 const form = document.querySelector(`form`);
+const nextButton = document.querySelector(`.next--button`);
+const prevButton = document.querySelector(`.prev--button`);
+console.log(nextButton);
 
 form.addEventListener(`submit`, (e) => {
   e.preventDefault();
@@ -12,6 +15,11 @@ form.addEventListener(`submit`, (e) => {
 inputMouseOver(inputs1);
 
 submitButton.addEventListener(`click`, (e) => {
+  e.preventDefault();
+  submit(e, inputs1);
+});
+
+nextButton.addEventListener(`click`, (e) => {
   e.preventDefault();
   submit(e, inputs1);
 });
